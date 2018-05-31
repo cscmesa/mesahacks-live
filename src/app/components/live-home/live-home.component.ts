3278456df97beb9d4d230ffdb6c6c6485b127a7e
devonrojas <devonmrojas@gmail.com>
2018-05-31 11:11:42 -0700

    Final adds

--- src/app/components/live-home/live-home.component.ts
+++ src/app/components/live-home/live-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
 import { Observable } from 'rxjs';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-live-home',
@@ -16,8 +17,7 @@ export class LiveHomeComponent implements OnInit {
   'Make sure you join our Slack workspace for the latest updates!',
   'Have you started your TwilioQuest? Check out the Prizes section for the link!',
   "Stuck on your code? Don't know what to do next? Ask a mentor in our #help channel on Slack!",
-  "Don't forget! Hacking ends at 7:30PM!",
-  ''
+  "Don't forget! Hacking ends at 7:30PM!"
   ];
   msg: string;
   index: number;
@@ -33,6 +33,11 @@ export class LiveHomeComponent implements OnInit {
       this.index += 1;
     });
 
+  }
+
+  passShow() {
+    $('#pass').html('mesaspring2018');
+    $('#pass').removeClass('pass');
   }
 
 }
